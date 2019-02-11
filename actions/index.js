37b4dc58e13ff88f0c4be59da69cdf7a55862a72
taehyunlim <taehyun.lim@gmail.com>
2019-02-10 22:21:46 -0800

    Receives Deck

--- actions/index.js
+++ actions/index.js
@@ -3,11 +3,19 @@ export const REMOVE_DECK = "REMOVE_DECK";
 export const ADD_CARD = "ADD_CARD";
 export const REMOVE_CARD = "REMOVE_CARD";
 export const RECEIVE_DECKS = "RECEIVE_DECKS"
+export const RECEIVE_DECK = "RECEIVE_DECK"
 
 export function receiveDecks(decks) {
   return {
     type: RECEIVE_DECKS,
     decks
+  };
+}
+
+export function receiveDeck(deck) {
+  return {
+    type: RECEIVE_DECK,
+    deck
   };
 }
 
